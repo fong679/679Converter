05a281adbf1afc7f6509e642f179a8cce0e599f3
fong679 <obacaufong@gmail.com>
2026-02-04 16:10:57 +1200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = '679converter-v1';
+const urlsToCache = [
+  '/679Converter/',
+  '/679Converter/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
